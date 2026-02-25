e1a57f9bd70535d291dba61bcaffb3b605802783
Ondrej Hejda <ondrejh.ck@email.cz>
2026-02-25 18:28:04 +0100

    naive fullmtb.bin gluing

---
+++ tesla/copy_images.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp ../extern/hacking-lidl-silvercrest-gateway/3-Main-SoC-Realtek-RTL8196E/31-Bootloader/boot.bin .
+cp ../extern/hacking-lidl-silvercrest-gateway/3-Main-SoC-Realtek-RTL8196E/32-Kernel/kernel.img .
+cp ../extern/hacking-lidl-silvercrest-gateway/3-Main-SoC-Realtek-RTL8196E/33-Rootfs/rootfs.bin .
+cp ../extern/hacking-lidl-silvercrest-gateway/3-Main-SoC-Realtek-RTL8196E/34-Userdata/userdata.bin .
